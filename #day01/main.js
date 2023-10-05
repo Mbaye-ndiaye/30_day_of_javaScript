c64bfd316166a929c67e2f86fcea075aefaaa388
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-10-05 20:54:30 +0000

    #day01

---
+++ #day01/main.js
@@ -0,0 +1,5 @@
+// the variables values can be accessed from other variable.js file
+
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boillingpoint, PI);
+console.log(name, job, live );
