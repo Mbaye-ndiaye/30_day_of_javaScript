ea2713a6cd2348bd6a9448a05dedac8702b559f1
Mbaye Ndiaye <babacarndiay546@gmail.com>
2023-10-10 23:58:13 +0000

    Day06

---
+++ 06_Day_Boucle/Exercices/Exercice01/main.js
@@ -0,0 +1,50 @@
+/*Itérer de 0 à 10 en utilisant la boucle for, 
+faire de même en utilisant la boucle while et faire while
+*/
+
+// for (let i = 0; i <= 10; i++){
+//     console.log(i);
+// }
+
+//  let i = 0;
+//  while (i <= 10) {
+//      console.log(i);
+
+//      i++  
+//  }
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++
+    
+// } while (i <= 10);
+
+
+// for (let i = 10; i >= 0; i--){
+//     console.log(i);
+// }
+
+
+// let i = 10;
+// while (i >= 0){
+//     console.log(i);
+//     i--
+// }
+
+
+// let i = 10; 
+// do {
+//     console.log(i);
+//     i--
+// } while(i >= 0)
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`${i} * ${i} = ${i * i}`);
+    
+// }
+
+for (let i = 0; i <= 7; i++){
+
+    console.log(`${i}\n + #`);
+}
